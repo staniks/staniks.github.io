e12ac9ea7e1d263cca600a7fcd14537574ac5f07
Marko Stanić <mstani19@gmail.com>
2019-07-29 19:52:25 +0200

    Update web.

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for filename in markdown/*.md; do
+    stripped_name="$(basename -- $filename)"
+    stripped_name="${stripped_name%%.*}"
+    cat html/header.html > "$stripped_name".html
+    echo $(Markdown.pl "$filename") >> "$stripped_name".html
+    cat html/footer.html >> "$stripped_name".html
+done
